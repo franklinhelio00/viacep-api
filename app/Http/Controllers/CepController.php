23cd1976dbb089d2fb191a648f4b77befa2120c9
franklinhelio00 <franklinhelio00@gmail.com>
2024-07-19 13:01:14 -0300

    Adicionar anotações do Swagger

--- app/Http/Controllers/CepController.php
+++ app/Http/Controllers/CepController.php
@@ -7,31 +7,68 @@ use Illuminate\Support\Facades\Http;
 
 class CepController extends Controller
 {
+    /**
+     * @OA\Get(
+     *     path="/search/local/{ceps}",
+     *     summary="Consulta múltiplos CEPs",
+     *     @OA\Parameter(
+     *         name="ceps",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string"
+     *         ),
+     *         description="Lista de CEPs separados por vírgula"
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Retorna os dados dos CEPs",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(
+     *                 type="object",
+     *                 @OA\Property(property="cep", type="string"),
+     *                 @OA\Property(property="label", type="string"),
+     *                 @OA\Property(property="logradouro", type="string"),
+     *                 @OA\Property(property="complemento", type="string"),
+     *                 @OA\Property(property="bairro", type="string"),
+     *                 @OA\Property(property="localidade", type="string"),
+     *                 @OA\Property(property="uf", type="string"),
+     *                 @OA\Property(property="ibge", type="string"),
+     *                 @OA\Property(property="gia", type="string"),
+     *                 @OA\Property(property="ddd", type="string"),
+     *                 @OA\Property(property="siafi", type="string")
+     *             )
+     *         )
+     *     )
+     * )
+     */
     public function search($ceps)
     {
         $cepsArray = explode(',', $ceps);
         $results = [];
 
         foreach ($cepsArray as $cep) {
-        // $response = Http::get("https://viacep.com.br/ws/{$cep}/json/"); // Ignore essa linha para desativar temporariamente a verificação do certificado SSL e ative a linha abaixo para conseguir testar em ambiente local
-        $response = Http::withOptions(['verify' => false])->get("https://viacep.com.br/ws/{$cep}/json/");
-           if ($response->successful()) {
-           $data = $response->json();
-           $results[] = [
-            'CEP' => str_replace('-', '', $data['cep']),
-            'Rótulo' => "{$data['logradouro']}, {$data['localidade']}",
-            'Logradouro' => $data['logradouro'],
-            'Complemento' => $data['complemento'],
-            'Bairro' => $data['bairro'],
-            'Cidade' => $data['localidade'],
-            'Estado' => $data['uf'],
-            'DDD' => $data['ddd'],
-            'IBGE' => $data['ibge'],
-            'Gia' => $data['gia'],
-            'Siafi' => $data['siafi'],
-           ];
-           }
+            // $response = Http::get("https://viacep.com.br/ws/{$cep}/json/"); // Ignore essa linha para desativar temporariamente a verificação do certificado SSL e ative a linha abaixo para conseguir testar em ambiente local
+            $response = Http::withOptions(['verify' => false])->get("https://viacep.com.br/ws/{$cep}/json/");
+            if ($response->successful()) {
+                $data = $response->json();
+                $results[] = [
+                    'cep' => str_replace('-', '', $data['cep']),
+                    'label' => "{$data['logradouro']}, {$data['localidade']}",
+                    'logradouro' => $data['logradouro'],
+                    'complemento' => $data['complemento'],
+                    'bairro' => $data['bairro'],
+                    'localidade' => $data['localidade'],
+                    'uf' => $data['uf'],
+                    'ibge' => $data['ibge'],
+                    'gia' => $data['gia'] ?? '',
+                    'ddd' => $data['ddd'],
+                    'siafi' => $data['siafi'],
+                ];
+            }
         }
+
         return response()->json($results);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,26 @@
 <?php
 
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="API de Consulta de CEP",
+ *     description="Documentação da API para consulta de múltiplos CEPs usando a API ViaCEP",
+ *     @OA\Contact(
+ *         email="seu-email@exemplo.com"
+ *     ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * )
+ */
+
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\CepController;
 
 Route::get('/search/local/{ceps}', [CepController::class, 'search']);
 
+Route::get('api/documentation', function () {
+    return view('swagger.index');
+});
